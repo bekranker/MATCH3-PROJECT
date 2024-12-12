460f7aa63fc6a366afc8e9c67deafa4f98e8b465
Bekir <aqwwwe73@gmail.com>
2024-12-12 23:31:11 +0300

    responsive position added, next step is adding responsive size

--- MyGrid.cs
+++ MyGrid.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class MyGrid : MonoBehaviour, IPoolObject
 {
+    [Header("---Props")]
     [SerializeField] private Vector2Int _GridSize;
     [SerializeField] private GameObject _CellPrefab;
+    [Header("---Components")]
     [SerializeField] private Pool _Pool;
-    [SerializeField] private Vector2 _Offset;
+    [SerializeField] private ResponsiveCamera _RPCam;
     void Start()
     {
         SetGrid();
     }
+
+    //The reason we added 1.5f is because Cell covers the Sprite in 1.5f space. Its durability (i.e. xPlus on X, yPlus on Y) should be increased by 1.5f for each axis and added to the current cell bead.
     void SetGrid()
     {
         float yPlus = 0, xPlus = 0;
@@ -20,10 +24,11 @@ public class MyGrid : MonoBehaviour, IPoolObject
             {
                 xPlus += 1.5f;
                 GameObject tempGrid = _Pool.GetFromPool(_CellPrefab);
-                SetPos(new Vector2(_Offset.x - (x + xPlus), _Offset.y - (y + yPlus)), tempGrid.transform);
+                SetPos(new Vector2(x + xPlus, y + yPlus), tempGrid.transform);
             }
             xPlus = 0;
         }
+        _RPCam.SetCamPosAndSize(_GridSize);
     }
     void SetPos(Vector2 currentCellPos, Transform cellT)
     {
--- ResponsiveCamera.cs
+++ ResponsiveCamera.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class ResponsiveCamera : MonoBehaviour
 {
+    [SerializeField] private Camera _MainCamera;
+
+    //It is the responsive function.;
+    public void SetCamPosAndSize(Vector2Int gridSize)
+    {
+        Vector3 tempPos = new Vector3((gridSize.x * 2.5f) / 2, (gridSize.y * 2.5f) / 2, -10);
+        _MainCamera.transform.position = tempPos;
+    }
 
 }
